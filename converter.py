7467cd6db7eb80556500db8486c2aa1bb0200cb2
SebDaBaBo <sebastian.a.rein@icloud.com>
2024-07-10 17:48:50 +0200

    v2

--- converter.py
+++ converter.py
@@ -1,12 +1,20 @@
+import string
+
 def convert_base(number, from_base, to_base):
-    decimal = int(str(number), from_base)
-    
+    digits = string.digits + string.ascii_letters + '+/'
+
+    if from_base != 10:
+        decimal = 0
+        power = 0
+        for digit in reversed(str(number)):
+            decimal += digits.index(digit) * (from_base ** power)
+            power += 1
+    else:
+        decimal = int(number)
+
     if to_base == 10:
         return str(decimal)
-    elif to_base == 16:
-        return hex(decimal)[2:].upper()
     else:
-        digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         result = ""
         while decimal > 0:
             result = digits[decimal % to_base] + result
@@ -15,11 +23,11 @@ def convert_base(number, from_base, to_base):
 
 while True:
     number = input("Enter the number to convert: ")
-    from_base = int(input("Enter the base of the input number (2-36): "))
-    to_base = int(input("Enter the base to convert to (2-36): "))
+    from_base = int(input("Enter the base of the input number (2-64): "))
+    to_base = int(input("Enter the base to convert to (2-64): "))
 
-    if from_base < 2 or from_base > 36 or to_base < 2 or to_base > 36:
-        print("Base must be between 2 and 36")
+    if from_base < 2 or from_base > 64 or to_base < 2 or to_base > 64:
+        print("Base must be between 2 and 64")
         continue
 
     result = convert_base(number, from_base, to_base)
